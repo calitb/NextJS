6259c242f6d618e55b6bd86769a4816fad10fa84
Carlos Thurber B <thurber.carlos@gmail.com>
2021-01-16 17:42:34 -0500

    custom homepage

---
+++ pages/index.tsx
@@ -0,0 +1,33 @@
+import Head from 'next/head';
+import Link from "next/link";
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Rick and Morty</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className="text-4xl">
+          Rick and Morty
+        </h1>
+        <Link href="/ssg/characters">
+          <a className="underline hover:text-red-400">
+            Static Site Generator
+          </a>
+        </Link>
+
+        <Link href="/ssr/characters">
+          <a className="underline hover:text-red-400">
+            Server Side Render
+          </a>
+        </Link>
+
+      </main>
+
+    </div>
+  )
+}
